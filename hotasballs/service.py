ac131dbe3c5ca65c0dd9e7b63e80e66c50252d4c
Francois Rejete <francois@rejete.com>
2019-06-06 16:09:11 +0900

    use the city definition from the event

--- hotasballs/service.py
+++ hotasballs/service.py
@@ -23,9 +23,9 @@ def handler(event, context):
     humidity = today['humidity']
 
     if apparentTemperatureHigh < 35:
-        message = "It's not that hot in Tokyo today, chill out."
+        message = f"It's not that hot in {city} today, chill out."
     else:
-        message = "Man, it's hot as balls in Tokyo today!"
+        message = f"Man, it's hot as balls in {city} today!"
         if apparentTemperatureHigh - temperatureHigh > 1:
             message += f"\n{temperatureHigh:.1f}°C ({apparentTemperatureHigh:.1f}°C felt) with {humidity * 100:.0f}% humidity"
         else:
